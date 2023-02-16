7d74b8ab1c19e4daff571fa05449c4633842e5cd
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-02-15 22:21:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Typescript</h2>
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todos.tsx
@@ -0,0 +1,13 @@
+function Todos() {
+  return (
+    <div>
+      <ul>
+        <li>Learn React</li>
+        <li>Learn Typescript</li>
+        <li>Learn MongoDB</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Todos;
